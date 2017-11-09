93982eed047431c67bd96d45eda11528b81b5c0c
Victor Garcia <vgarciabnz@gmail.com>
2017-11-09 12:47:59 +0300

    Clean some code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Settings } from './services/Settings';
 import { AppList } from './components/AppList';
 
 class App extends Component {
-
-  settings = new Settings();
-
-  constructor(props){
-    super(props);
-  }
-
   render() {
     return (
       <div className="App">
